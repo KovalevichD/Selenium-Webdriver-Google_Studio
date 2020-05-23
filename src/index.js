a7083035a14dc87d5a98de62c343a33af0c79f1c
Mac <mac@Macs-MacBook-Pro.local>
2020-05-23 22:49:45 +0300

    added checking backup image

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 const myEmail = 'email'
 const myPassword = 'password'
 
-const {Builder, By, Key, until} = require('selenium-webdriver')
+const {
+  Builder,
+  By,
+  Key,
+  until
+} = require('selenium-webdriver')
 const path = require('path')
 const fs = require('fs')
 const chalk = require('chalk')
@@ -51,14 +56,14 @@ fs.readdirSync(cuurentDir).forEach(creative => {
 
     let stats = fs.statSync(filePath)
     let fileSizeInKBytes = stats["size"]
-    
+
     creativesInfo.folderSize += fileSizeInKBytes
   })
 
   creativesInfo.folderSize = Math.round(creativesInfo.folderSize / 1000) + 'KB'
 
   //creating string of files for multiple input.SendKeys
-  for(let i = 0; i < pathsOfFilesArr.length; i++) {
+  for (let i = 0; i < pathsOfFilesArr.length; i++) {
     i !== pathsOfFilesArr.length - 1 ? filesInString = filesInString + pathsOfFilesArr[i] + '\n' : filesInString = filesInString + pathsOfFilesArr[i]
   }
 
@@ -85,9 +90,9 @@ async function run(store, updateOnly) {
     durationObj.startUploadCreativeTime = new Date().getTime()
 
     let needToUpload = isNeedToUpload(creative.creativeName, updateOnly)
-    
-    if(updateOnlySpecified && !needToUpload) continue
- 
+
+    if (updateOnlySpecified && !needToUpload) continue
+
     let creativeIsStillThere = await isCreaviseAlreadyCreated(creative.creativeName)
 
     if (creativeIsStillThere) {
@@ -95,18 +100,18 @@ async function run(store, updateOnly) {
     } else {
       await uploadCreative(creative, uploadMessage)
     }
-  } 
+  }
 
   durationObj.endProgramTime = new Date().getTime()
-  
+
   const totalDuration = getDuration(durationObj.startProgramTime, durationObj.endProgramTime)
   console.log(`Program execution time - ${chalk.bold.blue(totalDuration)}\n`)
 }
 
 async function loginToGoogleAccount(email, password) {
-  await driver.manage().window().maximize()  
+  await driver.manage().window().maximize()
   await driver.get('https://accounts.google.com/')
-  
+
   let byGoogleAccountEmail = By.name('identifier');
   await driver.wait(until.elementLocated(byGoogleAccountEmail, 30000));
   let googleAccountEmail = driver.findElement(byGoogleAccountEmail);
@@ -120,7 +125,7 @@ async function loginToGoogleAccount(email, password) {
   googleAccountPassword.sendKeys(password, Key.RETURN)
 
   await driver.wait(until.titleIs('Google Account'), 10000);
-  
+
   await driver.get('https://www.google.com/doubleclick/studio/#advertisers:')
 }
 
@@ -156,16 +161,16 @@ async function goToCampaignsTab(advertiser, campaign) {
   } catch (e) {
     campaignIsStillThere = false;
   }
-  
-  if(campaignIsStillThere) {
+
+  if (campaignIsStillThere) {
     await driver.findElement(By.xpath(`//*[text()='${campaign}']`)).click()
   } else {
     await driver.findElement(By.id('gwt-debug-table-button')).click()
 
     //check if the fields of some input are not still empty. Because AJAX is used
-    while(true) {
+    while (true) {
       let checkInputsAreNotEmpty = await driver.executeScript('let inputAccountName = document.getElementById("gwt-debug-new-campaign-accountText"); let inputAdvertiserName = document.getElementById("gwt-debug-new-campaign-advertiserText"); return inputAccountName.value && inputAdvertiserName.value')
-      
+
       if (checkInputsAreNotEmpty) break
     }
 
@@ -177,7 +182,7 @@ async function goToCampaignsTab(advertiser, campaign) {
     inputCreateCampaign.sendKeys(store.campaign)
 
     await driver.findElement(By.id('gwt-debug-save-button')).click()
-  }  
+  }
 }
 
 function isNeedToUpload(creativeFullName, updateOnly) {
@@ -189,7 +194,7 @@ function isNeedToUpload(creativeFullName, updateOnly) {
 
     if (isNeedToUploadBoolean) return true
   }
-  
+
   return false
 }
 
@@ -216,7 +221,7 @@ async function isCreaviseAlreadyCreated(creative) {
   } catch (e) {
     creativeIsStillThere = false;
   }
-  
+
   return creativeIsStillThere
 }
 
@@ -224,7 +229,7 @@ async function updateCreative(creative, message) {
   await driver.findElement(By.xpath(`//*[text()='${creative.creativeName}']`)).click()
 
   const isTheCreativeNotEmpty = await isCreativeEmpty()
-  
+
   if (isTheCreativeNotEmpty) await deleteAllFilesOfCreative()
 
   await uploadFiles(creative.filesInString, creative.namesOfFilesArr, creative.pathsOfFilesArr, creative.creativeName, message, creative.numOfFiles, creative.folderSize)
@@ -240,7 +245,7 @@ async function isCreativeEmpty() {
   } catch (e) {
     deleteBtnIsStillThere = false;
   }
-  
+
   return deleteBtnIsStillThere
 }
 
@@ -266,86 +271,88 @@ async function uploadCreative(creative, message) {
 }
 
 async function createNewCreative(creativeName, creativeWidth, creativeHeight) {
-    //wait when the button "new creative" appears and click on it
-    let byNewCretive = By.id('gwt-debug-new-creative-dropdown-new-creative-button');
-    await driver.wait(until.elementLocated(byNewCretive, 30000));
-    let buttonNewCretive = driver.findElement(byNewCretive);
-    await driver.wait(until.elementIsVisible(buttonNewCretive), 30000);
-    buttonNewCretive.click()
-    
-    //waiting for the button "Next" to appear. Hence this means that the entire page has loaded
-    let byNewCreativeNext = By.id('gwt-debug-creativeworkflow-next-button');
-    await driver.wait(until.elementLocated(byNewCreativeNext, 30000));
-    let buttonNewCretiveNext = driver.findElement(byNewCreativeNext);
-    await driver.wait(until.elementIsVisible(buttonNewCretiveNext), 30000);
-  
-    //check if the fields of some input are not still empty. Because AJAX is used
-    while(true) {
-      let checkInputsAreNotEmpty = await driver.executeScript('let inputAccountName = document.getElementById("gwt-debug-creativeDetail-accountText"); let inputAdvertiserName = document.getElementById("gwt-debug-creativeDetail-advertiserText"); let inputCampaignName = document.getElementById("gwt-debug-creativeDetail-campaignText"); return inputAccountName.value && inputAdvertiserName.value && inputCampaignName.value')
-    
-      if (checkInputsAreNotEmpty) break
-    }
-  
-    //send keys in creative name input
-    let byCreativeName = By.id('gwt-debug-creativeDetail-nameText');
-    await driver.wait(until.elementLocated(byCreativeName, 30000));
-    let inputCreativeName = driver.findElement(byCreativeName);
-    await driver.wait(until.elementIsVisible(inputCreativeName), 30000);
-    inputCreativeName.sendKeys(creativeName)
-    
-    //click on the "size button" 
-    await driver.findElement(By.id('gwt-debug-creativeDetail-sizeText')).click()
-  
-    //choose user-defined dimensions option
-    let byUserDefined = By.id('gwt-debug-creativeDetail-sizeText-CUSTOM');
-    await driver.wait(until.elementLocated(byUserDefined, 30000));
-    let optionUserDefined = driver.findElement(byUserDefined);
-    await driver.wait(until.elementIsVisible(optionUserDefined), 30000);
-    optionUserDefined.click()
-  
-    //send keys in creative width input
-    let byCreativeWidth = By.id('gwt-debug-creativeDetail-widthText');
-    await driver.wait(until.elementLocated(byCreativeWidth, 30000));
-    let inputCreativeWidth = driver.findElement(byCreativeWidth);
-    await driver.wait(until.elementIsVisible(inputCreativeWidth), 30000);
-    inputCreativeWidth.sendKeys(creativeWidth)
-  
-    //send keys in creative height input
-    let byCreativeHeight = By.id('gwt-debug-creativeDetail-heightText');
-    await driver.wait(until.elementLocated(byCreativeHeight, 30000));
-    let inputCreativeHeight = driver.findElement(byCreativeHeight);
-    await driver.wait(until.elementIsVisible(inputCreativeHeight), 30000);
-    await inputCreativeHeight.sendKeys(creativeHeight)
-  
-    await buttonNewCretiveNext.click()
+  //wait when the button "new creative" appears and click on it
+  let byNewCretive = By.id('gwt-debug-new-creative-dropdown-new-creative-button');
+  await driver.wait(until.elementLocated(byNewCretive, 30000));
+  let buttonNewCretive = driver.findElement(byNewCretive);
+  await driver.wait(until.elementIsVisible(buttonNewCretive), 30000);
+  buttonNewCretive.click()
+
+  //waiting for the button "Next" to appear. Hence this means that the entire page has loaded
+  let byNewCreativeNext = By.id('gwt-debug-creativeworkflow-next-button');
+  await driver.wait(until.elementLocated(byNewCreativeNext, 30000));
+  let buttonNewCretiveNext = driver.findElement(byNewCreativeNext);
+  await driver.wait(until.elementIsVisible(buttonNewCretiveNext), 30000);
+
+  //check if the fields of some input are not still empty. Because AJAX is used
+  while (true) {
+    let checkInputsAreNotEmpty = await driver.executeScript('let inputAccountName = document.getElementById("gwt-debug-creativeDetail-accountText"); let inputAdvertiserName = document.getElementById("gwt-debug-creativeDetail-advertiserText"); let inputCampaignName = document.getElementById("gwt-debug-creativeDetail-campaignText"); return inputAccountName.value && inputAdvertiserName.value && inputCampaignName.value')
+
+    if (checkInputsAreNotEmpty) break
+  }
+
+  //send keys in creative name input
+  let byCreativeName = By.id('gwt-debug-creativeDetail-nameText');
+  await driver.wait(until.elementLocated(byCreativeName, 30000));
+  let inputCreativeName = driver.findElement(byCreativeName);
+  await driver.wait(until.elementIsVisible(inputCreativeName), 30000);
+  inputCreativeName.sendKeys(creativeName)
+
+  //click on the "size button" 
+  await driver.findElement(By.id('gwt-debug-creativeDetail-sizeText')).click()
+
+  //choose user-defined dimensions option
+  let byUserDefined = By.id('gwt-debug-creativeDetail-sizeText-CUSTOM');
+  await driver.wait(until.elementLocated(byUserDefined, 30000));
+  let optionUserDefined = driver.findElement(byUserDefined);
+  await driver.wait(until.elementIsVisible(optionUserDefined), 30000);
+  optionUserDefined.click()
+
+  //send keys in creative width input
+  let byCreativeWidth = By.id('gwt-debug-creativeDetail-widthText');
+  await driver.wait(until.elementLocated(byCreativeWidth, 30000));
+  let inputCreativeWidth = driver.findElement(byCreativeWidth);
+  await driver.wait(until.elementIsVisible(inputCreativeWidth), 30000);
+  inputCreativeWidth.sendKeys(creativeWidth)
+
+  //send keys in creative height input
+  let byCreativeHeight = By.id('gwt-debug-creativeDetail-heightText');
+  await driver.wait(until.elementLocated(byCreativeHeight, 30000));
+  let inputCreativeHeight = driver.findElement(byCreativeHeight);
+  await driver.wait(until.elementIsVisible(inputCreativeHeight), 30000);
+  await inputCreativeHeight.sendKeys(creativeHeight)
+
+  await buttonNewCretiveNext.click()
 }
 
 async function uploadFiles(filesInString, namesOfFilesArr, pathsOfFilesArr, creativeName, message, numOfFiles, folderSize) {
-    //waiting for the "drop zone" element
-    let byDropZone = By.id('gwt-debug-creativeworkflow-drop-zone');
-    await driver.wait(until.elementLocated(byDropZone, 30000));
-    let elementDropZone = driver.findElement(byDropZone);
-    await driver.wait(until.elementIsVisible(elementDropZone), 30000);
-  
-    //make visible input[type='file'] and upload files
-    await driver.executeScript('document.querySelector("input[type=file]").style.height = "50px"; document.querySelector("input[type=file]").style.width = "50px"; document.querySelector("input[type=file]").style.display="block"; document.querySelector("input[type=file]").style.visibility="visible";  ')
-    await driver.findElement(By.xpath("//input[@type='file']")).sendKeys(`${filesInString}`)
-   
-    //waiting for all files to load
-    let byUploadComplete = By.xpath('//*[text()="Upload complete"]');
-    await driver.wait(until.elementLocated(byUploadComplete, 120000));
-    let elementUploadComplete = driver.findElement(byUploadComplete);
-    await driver.wait(until.elementIsVisible(elementUploadComplete), 120000);
-  
-    await driver.sleep(2000)
-  
-    await checkAndReaploadFiledFiles(namesOfFilesArr, pathsOfFilesArr, creativeName, message, numOfFiles, folderSize)
-  
-    await driver.findElement(By.id('gwt-debug--breadcrumbs-link-2')).click()
-  
-    await driver.sleep(3000)
-}
+  //waiting for the "drop zone" element
+  let byDropZone = By.id('gwt-debug-creativeworkflow-drop-zone');
+  await driver.wait(until.elementLocated(byDropZone, 30000));
+  let elementDropZone = driver.findElement(byDropZone);
+  await driver.wait(until.elementIsVisible(elementDropZone), 30000);
 
+  //make visible input[type='file'] and upload files
+  await driver.executeScript('document.querySelector("input[type=file]").style.height = "50px"; document.querySelector("input[type=file]").style.width = "50px"; document.querySelector("input[type=file]").style.display="block"; document.querySelector("input[type=file]").style.visibility="visible";  ')
+  await driver.findElement(By.xpath("//input[@type='file']")).sendKeys(`${filesInString}`)
+
+  //waiting for all files to load
+  let byUploadComplete = By.xpath('//*[text()="Upload complete"]');
+  await driver.wait(until.elementLocated(byUploadComplete, 120000));
+  let elementUploadComplete = driver.findElement(byUploadComplete);
+  await driver.wait(until.elementIsVisible(elementUploadComplete), 120000);
+
+  await driver.sleep(2000)
+
+  await checkAndReaploadFiledFiles(namesOfFilesArr, pathsOfFilesArr, creativeName, message, numOfFiles, folderSize)
+  await driver.sleep(3000)
+  await checkBackup()
+
+  await driver.findElement(By.id('gwt-debug--breadcrumbs-link-2')).click()
+
+  await driver.sleep(3000)
+}
+//TODO check if it work correctly with .zip
 async function checkAndReaploadFiledFiles(namesOfFilesArr, pathsOfFilesArr, creativeName, message, numOfFiles, folderSize) {
   let strSrciptCheckFiledFiles = 'let parent = document.getElementById("gwt-debug-upload-panel-file-list"); let childsOfLastChildren = parent.getElementsByTagName("div")[1].children; let arr = [];for (let i = 0; i < childsOfLastChildren.length; i++) {let fileName = childsOfLastChildren[i].children[0].getAttribute("title");let uploadResult = childsOfLastChildren[i].children[1].children[1].innerHTML;if (uploadResult === "Failed") {arr.push(fileName)};}; return arr;'
   let resultArr = await driver.executeScript(strSrciptCheckFiledFiles)
@@ -353,22 +360,22 @@ async function checkAndReaploadFiledFiles(namesOfFilesArr, pathsOfFilesArr, crea
   if (resultArr.length) {
     for (let i = 0; i < resultArr.length; i++) {
       let indexOfFiledFile = namesOfFilesArr.indexOf(resultArr[i])
-    
+
       await driver.findElement(By.xpath("//input[@type='file']")).sendKeys(`${pathsOfFilesArr[indexOfFiledFile]}`)
       await driver.sleep(3000)
-      
+
       let recheckArr = await driver.executeScript(strSrciptCheckFiledFiles)
       const recheckArrWithoutDuplicates = recheckArr.filter((it, index) => index === recheckArr.indexOf(it = it.trim()))
 
       if (recheckArr.length) console.log(`\n${chalk.red('Not fully loaded')} => ${creativeName}\n Error loading the following files => ${chalk.red(recheckArrWithoutDuplicates)}\n`)
-      
-    } 
+
+    }
   } else {
-      durationObj.endUploadCreativeTime = new Date().getTime()
+    durationObj.endUploadCreativeTime = new Date().getTime()
 
-      let uploadingDuration = getDuration(durationObj.startUploadCreativeTime, durationObj.endUploadCreativeTime)
+    let uploadingDuration = getDuration(durationObj.startUploadCreativeTime, durationObj.endUploadCreativeTime)
 
-      console.log(`\n${chalk.green(`Creative ${message} `)}${chalk.blue(`(Time: ${uploadingDuration}, Files: ${numOfFiles}, Size: ${folderSize})`)} => ${creativeName}\n`)
+    console.log(`\n${chalk.green(`Creative ${message} `)}${chalk.blue(`(Time: ${uploadingDuration}, Files: ${numOfFiles}, Size: ${folderSize})`)} => ${creativeName}\n`)
   }
 }
 
@@ -378,4 +385,10 @@ function getDuration(start, end) {
   let result = `${durationMinutes}m ${Math.trunc(durationSeconds - (durationMinutes * 60))}s`
 
   return result
+}
+
+async function checkBackup() {
+  const checkBackupFunc = 'const size = document.getElementById("gwt-debug-dclk-creative-properties-size").innerHTML;const aTags = document.getElementsByTagName("a");const searchText = "backup_" + size + ".jpg";let found;for (let i = 0; i < aTags.length; i++) {if (aTags[i].textContent == searchText) {found = aTags[i];break;}}const link = found.parentElement.nextElementSibling.nextElementSibling.nextElementSibling.getElementsByTagName("a")[0].click()'
+
+  await driver.executeScript(checkBackupFunc)
 }
